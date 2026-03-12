89d06a6fbf1b7a9752506e463174ddc921d794ec
barkgoofball <allanzylee@gmail.com>
2026-03-11 23:43:15 -0400

    updates

--- Analysis/01_sum_stat/01_sum_stat.R
+++ Analysis/01_sum_stat/01_sum_stat.R
@@ -18,142 +18,170 @@ source("/Users/AllanLee/Desktop/Personal Projects/ECON4900/Code/Analysis/header.
 ###################################### Load relevant data ################################
 ##########################################################################################
 
-full_data_w <- read_rds('/Users/AllanLee/Desktop/Personal Projects/ECON4900/Data/build/regression_build_w.rds') %>% 
-  fastDummies::dummy_columns(c('e_cfies_scale',
-                               'e_fies_scale')) %>% 
-  dplyr::select(-e_cfies_scale,
-         -e_fies_scale)
+full_data_w <- read_rds('/Users/AllanLee/Desktop/Personal Projects/ECON4900/Data/build/regression_build_w.rds') %>%
+  fastDummies::dummy_columns(c('e_cfies_scale', 'e_fies_scale')) %>%
+  dplyr::select(-e_cfies_scale, -e_fies_scale) %>%
+  mutate(disagree = as.integer(e_cfies_indicator != e_fies_indicator))
 
-#########################################################################################
-######################################## Overall Summary Statistics ##############################
+
+##########################################################################################
+###################################### Helper: Summary Stat Function #####################
 ##########################################################################################
 
-summary_stat<-full_data_w %>% 
-  dplyr::select(
-                e_cfies_indicator,
-                e_fies_indicator,
-                contains('e_cfies_scale'),
-                contains('e_fies_scale'),
-                female,
-                age,
-                age_num,
-                contains('region_'),
-                matches('^e_.*per$')
-                )
-
-######################################## Overall Summary Statistics ##############################
-
-overall<-summary_stat %>% 
-  summarise(across(everything(),
-                .fns = list(
-    mean = mean, 
-    sd = sd))) %>% 
-  pivot_longer(cols = everything()) %>% 
-  mutate(statistic = str_match(name, pattern = ".+_(.+)")[,2],
-         var = str_match(name, pattern = "(.+)_.+")[,2]) %>% 
-  dplyr::select(-name) %>% 
-  pivot_wider(names_from = statistic, values_from = value)
-
-######################################## T-Test: Create function for group based summary statistics ##############################
-
-t_test_func<-function(var,
-                      group){
-  
-  exp <- expr(!!ensym(var) ~ !!ensym(group))
-  
-  test <- t.test(formula = eval(exp),
-         data=full_data_w)
-  
-  if(group=='female'){
-    
-    out<-tribble(~var,~male,~female,~pval_female,
-                 var,test$estimate[1],test$estimate[2],test$p.value)
-    
-  } else {
-    
-    out<-tribble(~var,~younger,~older,~pval_age,
-                 var,test$estimate[1],test$estimate[2],test$p.value)
-    
-  }
-  
-  return(out)
-  
+sum_stat_func <- function(var) {
+  tribble(
+    ~var, ~mean,                                                ~sd,
+    var,  mean(full_data_w %>% pull(!!ensym(var)), na.rm = T), sd(full_data_w %>% pull(!!ensym(var)), na.rm = T)
+  )
 }
 
-# Create function input
-input<-names(summary_stat) %>% 
-  as_tibble() %>% 
-  rename(var=value) %>% 
-  mutate(female='female',
-         age='age'
-         ) %>% 
-  filter(!str_detect(var,'e_cg_fs_dummy')) %>% 
-  filter(!str_detect(var,'e_fies_scale'))
 
-# Run function
-female_summary_stat<-pmap_dfr(input %>% dplyr::select(-age) %>% rename(group=female) %>% filter(var!='female'),
-                             t_test_func)
+##########################################################################################
+###################################### Run Summary Stats #################################
+##########################################################################################
 
-age_summary_stat<-pmap_dfr(input %>% dplyr::select(-female) %>% rename(group=age) %>% filter(var!='age_num',var!='age'),
-                              t_test_func)
-
-# Combine
-out<-overall %>% 
-  filter(var!='age',
-         !str_detect(var,'^m_.*.per$')) %>% 
-  left_join(female_summary_stat,
-            by=c('var')) %>% 
-  left_join(age_summary_stat,
-            by=c('var')) %>% 
-  rename(
-         'Mean'=mean,
-         'St. Dev.'=sd,
-         'Male'=male,
-         'Female'=female,
-         'P-Value: Child Sex'=pval_female,
-         '5-9 Years'=younger,
-         '10-17 Years'=older,
-         'P-Value: Child Age Group'=pval_age) %>% 
-  mutate(var=case_when(var=='e_cfies_indicator'~'Child-Reported FI (%)',
-                       var=='e_fies_indicator'~'Caregiver-Reported FI (%)',
-                       var=='e_cfies_scale_0'~'Child: No FI (CFIES=0)',
-                       var=='e_cfies_scale_1'~'Child: Few Exp. (CFIES=1-6)',
-                       var=='e_cfies_scale_2'~'Child: Several Exp. (CFIES=7-10)',
-                       var=='e_cfies_scale_3'~'Child: Many Exp. (CFIES=11-20)',
-                       var=='e_fies_scale_0'~'Caregiver: Food Secure (FIES=0-3)',
-                       var=='e_fies_scale_1'~'Caregiver: Moderately Food Insecure (FIES=4-6)',
-                       var=='e_fies_scale_2'~'Caregiver: Severely Food Insecure (FIES=7-8)',
-                       var=='female'~'Child is Female (%)',
-                       var=='age_num'~'Child Age (Years)',
-                       var=='region_north_east'~"Region: North East",
-                       var=='region_northern'~"Region: Northern",
-                       var=='region_savannah'~"Region: Savannah",
-                       var=='region_upper_east'~"Region: Upper East",
-                       var=='region_upper_west'~"Region: Upper West",
-                       var=='e_lit_per'~'Endline Literacy Z-Score',
-                       var=='e_num_per'~'Endline Numeracy Z-Score',
-                       var=='e_ef_per'~'Endline EF Z-Score',
-                       T~'Endline SEL Z-Score',
-                       )
-         ) %>% 
-  rename('Statistic'=var)
-
-# Create Latex Table
-latex=xtable(out, 
-             type = "latex")
-
-names(latex)=c("\\multicolumn{1}{p{2.5in}}{Statistic}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering Mean}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering St. Dev}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering Male}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering Female}",
-               "\\multicolumn{1}{p{0.5in}}{\\centering P-Value: Child Sex}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering 5-9 Years}",
-               "\\multicolumn{1}{p{0.25in}}{\\centering 10-17 Years}",
-               "\\multicolumn{1}{p{0.5in}}{\\centering P-Value: Child Age Group}"
-               )
-
-print(latex, sanitize.colnames.function=function(x){x},
-      include.rownames=FALSE)
+summed <- map_dfr(
+  c(
+    # Panel A: Food Insecurity Experiences
+    'e_cfies_indicator',
+    'e_fies_indicator',
+    'disagree',
+    'e_cfies_scale_0',
+    'e_cfies_scale_1',
+    'e_cfies_scale_2',
+    'e_cfies_scale_3',
+    'e_fies_scale_0',
+    'e_fies_scale_1',
+    'e_fies_scale_2',
+    # Panel B: Child Characteristics
+    'female',
+    'age_num',
+    'e_lit_per_raw',
+    'e_num_per_raw',
+    'e_ef_per_raw',
+    'e_sel_per_raw',
+    # Panel C: Region Characteristics
+    'region_north_east',
+    'region_northern',
+    'region_savannah',
+    'region_upper_east',
+    'region_upper_west'
+  ),
+  sum_stat_func
+) %>%
+  # Format numbers
+  mutate(
+    across(
+      -var,
+      ~ case_when(
+        str_detect(var, 'age_num') ~ formatC(.x, format = "f", big.mark = ",", digits = 1),
+        TRUE                       ~ formatC(.x, format = "f", big.mark = ",", digits = 3)
+      )
+    )
+  ) %>%
+  # Relabel variables
+  mutate(var = recode(
+    var,
+    'e_cfies_indicator'   = 'Child-Reported FI (\\%)',
+    'e_fies_indicator'    = 'Caregiver-Reported FI (\\%)',
+    'disagree'            = 'Child-Caregiver FI Disagreement (\\%)',
+    'e_cfies_scale_0'     = 'Child: No FI (CFIES=0)',
+    'e_cfies_scale_1'     = 'Child: Few Exp. (CFIES=1--6)',
+    'e_cfies_scale_2'     = 'Child: Several Exp. (CFIES=7--10)',
+    'e_cfies_scale_3'     = 'Child: Many Exp. (CFIES=11--20)',
+    'e_fies_scale_0'      = 'Caregiver: Food Secure (FIES=0--3)',
+    'e_fies_scale_1'      = 'Caregiver: Moderately Food Insecure (FIES=4--6)',
+    'e_fies_scale_2'      = 'Caregiver: Severely Food Insecure (FIES=7--8)',
+    'female'              = 'Child is Female (\\%)',
+    'age_num'             = 'Child Age (Years)',
+    'e_lit_per_raw'       = 'Literacy (\\% Correct)',
+    'e_num_per_raw'       = 'Numeracy (\\% Correct)',
+    'e_ef_per_raw'        = 'EF (\\% Correct)',
+    'e_sel_per_raw'       = 'SEL (\\% Correct)',
+    'region_north_east'   = 'North East',
+    'region_northern'     = 'Northern',
+    'region_savannah'     = 'Savannah',
+    'region_upper_east'   = 'Upper East',
+    'region_upper_west'   = 'Upper West'
+  ))
 
 
+##########################################################################################
+###################################### Build LaTeX Table #################################
+##########################################################################################
+
+make_row <- function(df, i) {
+  paste0(
+    df$var[i],  " & ",
+    df$mean[i], " & ",
+    df$sd[i],   " \\\\\n"
+  )
+}
+
+# Row index reference:
+# Panel A (Food Insecurity):       rows 1-10  (added disagree at row 3)
+# Panel B (Child Characteristics): rows 11-16
+# Panel C (Region):                rows 17-21
+
+panel_a_rows <- paste0(
+  # Indicator rates (rows 1-3: child FI, caregiver FI, disagreement)
+  paste0(sapply(1:3, function(i) make_row(summed, i)), collapse = ""),
+  "\\addlinespace\n",
+  # Categorical FI definitions (rows 4-10)
+  paste0(sapply(4:10, function(i) make_row(summed, i)), collapse = "")
+)
+
+panel_b_rows <- paste0(sapply(11:16, function(i) make_row(summed, i)), collapse = "")
+panel_c_rows <- paste0(sapply(17:21, function(i) make_row(summed, i)), collapse = "")
+
+# N row
+n_row <- paste0(
+  "Total Observations & ",
+  formatC(nrow(full_data_w), big.mark = ","),
+  " & \\\\\n"
+)
+
+latex_table <- paste0(
+  "\\begin{table}[H]\n",
+  "\\caption{Summary Statistics}\n",
+  "\\label{tab:sumstats}\\small\n",
+  "\\begin{tabular*}{\\textwidth}{@{\\extracolsep{\\fill}}lcc@{}}\n",
+  "\\toprule\n",
+  " & \\multicolumn{1}{c}{(1)} & \\multicolumn{1}{c}{(2)} \\\\\n",
+  " & Mean & Std. Dev. \\\\\n",
+  "\\midrule \\addlinespace\n",
+  
+  # Panel A
+  "\\multicolumn{3}{@{}l}{\\emph{Panel A: Food Insecurity Experiences}} \\\\ \\addlinespace\n",
+  panel_a_rows,
+  "\\addlinespace \\midrule \\addlinespace\n",
+  
+  # Panel B
+  "\\multicolumn{3}{@{}l}{\\emph{Panel B: Child Characteristics}} \\\\ \\addlinespace\n",
+  panel_b_rows,
+  "\\addlinespace \\midrule \\addlinespace\n",
+  
+  # Panel C
+  "\\multicolumn{3}{@{}l}{\\emph{Panel C: Region}} \\\\ \\addlinespace\n",
+  panel_c_rows,
+  "\\addlinespace \\midrule \\addlinespace\n",
+  
+  # N
+  n_row,
+  
+  "\\bottomrule\n",
+  "\\end{tabular*}\n",
+  "\\begin{spacing}{1}\n",
+  "\\begin{tablenotes}\n",
+  "  \\item \\footnotesize \\textit{Notes:} This table reports unweighted summary statistics.",
+  " FI = Food Insecure. CFIES = Child Food Insecurity Experience Scale. FIES = Food Insecurity Experience Scale.",
+  " Disagreement = 1 when child-reported and caregiver-reported food insecurity status differ.",
+  " Outcome variables are reported as raw percentage correct.",
+  " Region shares sum to one across the five northern regions of Ghana.\n",
+  "\\end{tablenotes}\n",
+  "\\end{spacing}\n",
+  "\\end{table}\n"
+)
+
+writeLines(latex_table,
+           "/Users/AllanLee/Desktop/Personal Projects/ECON4900/Output/01_sum_stat/01_sum_stat.tex")
--- Analysis/01_sum_stat/03_cronbach_alpha.R
+++ Analysis/01_sum_stat/03_cronbach_alpha.R
@@ -21,88 +21,110 @@ library(haven)
 library(writexl)
 
 # Load relevant data
+full_data_w <- read_rds('/Users/AllanLee/Desktop/Personal Projects/ECON4900/Data/build/regression_build_w.rds')
+
 m_child <- read_dta("import/03_PNP_Midline_ChildSurvey.dta") %>% 
-  mutate(across(contains('id'),~as.double(.)))
+  mutate(across(contains('id'),~as.double(.))) %>% 
+  filter(childid %in% unique(full_data_w$childid))
 e_child <- read_dta("import/03_PNP_Endline_ChildSurvey.dta") %>% 
   rename(careid=caseid) %>% 
-  mutate(across(contains('id'),~as.double(.)))
+  mutate(across(contains('id'),~as.double(.))) %>% 
+  filter(childid %in% unique(full_data_w$childid))
 
-
-###################################### Create function to calculate Cronbach's Alpha #############################
-get_alpha <- function(df){
-  
-  for_alpha<-get(df) %>% 
-    mutate(across(everything(),
-                  ~as.double(.)))
-  out<-as.double(alpha(for_alpha,check.keys=T)[['total']][['raw_alpha']]) %>% 
-    as_tibble() %>% 
-    mutate(test=df)
-  return(out)
-}
 
 ##########################################################################################
-###################################### Outcome Data: Cronbach's Alpha #############################
+###################################### Helpers ###########################################
+##########################################################################################
+
+# Coerces all columns to double and replaces negatives with NA
+clean_items <- function(df) {
+  df %>%
+    mutate(across(everything(), ~as.double(.))) %>%
+    mutate(across(everything(), ~ifelse(. < 0, NA, .)))
+}
+
+# Safely runs alpha() — returns NA with a warning if fewer than 2 items or alpha errors out
+safe_alpha <- function(df) {
+  tryCatch({
+    if (ncol(df) < 2) {
+      warning("Fewer than 2 items — alpha cannot be computed. Returning NA.")
+      return(NA_real_)
+    }
+    alpha(df, check.keys = TRUE)$total$raw_alpha
+  }, error = function(e) {
+    warning(paste("alpha() failed:", e$message, "— returning NA."))
+    NA_real_
+  })
+}
+
+
+##########################################################################################
+###################################### Outcome Data: Cronbach's Alpha ####################
 ##########################################################################################
 
 ############################################### Endline: SEL ######################################
+# CR items: binary (recode 2->0, 1->1)
+# RE items: ordinal; 6 items for age < 10, 9 items for age >= 10
+# Strategy: pool all SEL items per age group for a single category-level alpha.
 
-e_sel <- e_child %>% 
-  # dplyr::select relevant numeracy questions
-  dplyr::select(careid,
-                childid,
-                cr1:re11,
-                child_age=childage) %>%
-  # Dedplyr::select friends question
-  dplyr::select(-re5,-re8) %>% 
-  # Change all columns to numeric
-  mutate(across(everything(),~as.double(.))) %>% 
-  # Turn negatives into NA
-  mutate_all((~ifelse(. < 0, NA, .))) %>% 
-  # Change CR SEL questions to be 1/0 binary
-  mutate(across(contains('cr'),~case_when(.==2~0,
-                                          .==1~1,
-                                          T~NA_real_))) %>% 
-  summarise(cr_perc=rowSums(across(contains('cr')),na.rm=T)/length(colnames(e_child %>% select(contains('cr')))),
-         re_perc=case_when(child_age<10~rowSums(across(matches("re[0-9]")),na.rm=T)/6,
-                           T~rowSums(across(matches("re[0-9]")),na.rm=T)/9))
+e_sel_all <- e_child %>%
+  dplyr::select(childid, child_age = childage,
+                starts_with("cr"),
+                re1, re2, re3, re4, re6, re7, re9, re10, re11) %>%
+  clean_items() %>%
+  mutate(across(starts_with("cr"), ~case_when(. == 1 ~ 1,
+                                              . == 2 ~ 0,
+                                              TRUE   ~ NA_real_)))
 
-cor(e_sel$cr_perc,
-    e_sel$re_perc)
+# Age < 10: CR + 6 RE items (re9/re10/re11 not administered)
+alpha_sel_young <- e_sel_all %>%
+  filter(child_age < 10) %>%
+  dplyr::select(-childid, -child_age, -re9, -re10, -re11) %>%
+  safe_alpha()
 
-############################################### Endline Literacy ######################################
+# Age >= 10: CR + 9 RE items
+alpha_sel_old <- e_sel_all %>%
+  filter(child_age >= 10) %>%
+  dplyr::select(-childid, -child_age) %>%
+  safe_alpha()
 
-e_lit <- e_child %>% 
-  # dplyr::select relevant literacy questions
-  dplyr::select(careid,
-                childid,
-                ov1,
-                # starts_with("ov"),
-                starts_with("nr"),
+
+############################################### Endline: Literacy ######################################
+# All literacy items are binary. Single alpha across all literacy items (age-invariant).
+
+alpha_lit <- e_child %>%
+  dplyr::select(starts_with("nr"),
                 starts_with("sp"),
                 starts_with("or"),
                 starts_with("oc"),
-                matches("pa[0-9]"),
-                child_age=childage) %>% 
-  # Change all columns to numeric
-  mutate(across(everything(),~as.double(.))) %>% 
-  # Turn negatives into NA
-  mutate_all((~ifelse(. < 0, NA, .))) %>% 
-  # Given that all literacy variables are binary, they can be added together and calculated as a percentage.
-  summarise(nr_perc=rowSums(across(starts_with("ov")),na.rm=T)/15,
-            nr_perc=rowSums(across(starts_with("nr")),na.rm=T)/length(colnames(e_child %>% select(starts_with("nr")))),
-            sp_perc=rowSums(across(starts_with("sp")),na.rm=T)/length(colnames(e_child %>% select(starts_with("sp")))),
-            or_perc=rowSums(across(starts_with("or")),na.rm=T)/length(colnames(e_child %>% select(starts_with("or")))),
-            oc_perc=rowSums(across(starts_with("oc")),na.rm=T)/length(colnames(e_child %>% select(starts_with("oc")))),
-            pa_perc=rowSums(across(matches("pa[0-9]")),na.rm=T)/length(colnames(e_child %>% select(matches("pa[0-9]")))))
+                matches("pa[0-9]")) %>%
+  clean_items() %>%
+  safe_alpha()
+
 
 ############################################### Endline: Numeracy ######################################
-# Note that within the literacy category, there are 53 questions for 10-17 year olds and 47 for 5-9 year olds
+# Binary items. NU items only given to age >= 10, so split by age group.
 
-e_num <- e_child %>% 
-  # dplyr::select relevant numeracy questions
-  dplyr::select(careid,
-                childid,
-                matches("co[0-9]"),
+# Age < 10: all numeracy items except nu
+alpha_num_young <- e_child %>%
+  mutate(child_age = as.double(childage)) %>%
+  filter(child_age < 10) %>%
+  dplyr::select(matches("co[0-9]"),
+                starts_with("nd"),
+                starts_with("mn"),
+                starts_with("wp"),
+                starts_with("ad"),
+                matches("su[0-9]"),
+                starts_with("mu"),
+                matches("di[0-9]")) %>%
+  clean_items() %>%
+  safe_alpha()
+
+# Age >= 10: all numeracy items including nu
+alpha_num_old <- e_child %>%
+  mutate(child_age = as.double(childage)) %>%
+  filter(child_age >= 10) %>%
+  dplyr::select(matches("co[0-9]"),
                 starts_with("nd"),
                 starts_with("mn"),
                 starts_with("nu"),
@@ -110,60 +132,60 @@ e_num <- e_child %>%
                 starts_with("ad"),
                 matches("su[0-9]"),
                 starts_with("mu"),
-                matches("di[0-9]"),
-                child_age=childage) %>%
-  # Change all columns to numeric
-  mutate(across(everything(),~as.double(.))) %>% 
-  # Turn negatives into NA
-  mutate_all((~ifelse(. < 0, NA, .))) %>% 
-  # Given that all numeracy variables are binary, they can be added together and calculated as a percentage.
-  summarise(co_perc=rowSums(across(matches("co[0-9]")),na.rm=T)/length(colnames(e_child %>% select(matches("co[0-9]")))),
-            nd_perc=rowSums(across(starts_with("nd")),na.rm=T)/length(colnames(e_child %>% select(starts_with("nd")))),
-            mn_perc=rowSums(across(starts_with("mn")),na.rm=T)/length(colnames(e_child %>% select(starts_with("mn")))),
-            nu_perc=case_when(child_age>=10~rowSums(across(starts_with("nu")),na.rm=T)/length(colnames(e_child %>% select(starts_with("nu")))),
-                              T~NA),
-            wp_perc=rowSums(across(starts_with("wp")),na.rm=T)/length(colnames(e_child %>% select(starts_with("wp")))),
-            ad_perc=rowSums(across(starts_with("ad")),na.rm=T)/length(colnames(e_child %>% select(starts_with("ad")))),
-            su_perc=rowSums(across(matches("su[0-9]")),na.rm=T)/length(colnames(e_child %>% select(matches("su[0-9]")))),
-            mu_perc=rowSums(across(starts_with("mu")),na.rm=T)/length(colnames(e_child %>% select(starts_with("mu")))),
-            di_perc=rowSums(across(matches("di[0-9]")),na.rm=T)/length(colnames(e_child %>% select(matches("di[0-9]")))))
+                matches("di[0-9]")) %>%
+  clean_items() %>%
+  safe_alpha()
+
 
 ############################################### Endline: EF ######################################
-# Note that within the executive function category, there are 11 questions for 5-9 year olds and 17 questions for 10-17 year olds.
+# Pool WM + SM items per age group for a single category-level alpha.
 
-e_ef <- e_child %>% 
-  dplyr::select(childid,
-                starts_with("wm"),
-                starts_with("sm"),
-                child_age=childage) %>%
-  # Change all columns to numeric
-  mutate(across(everything(),~as.double(.))) %>% 
-  # Turn negatives into NA
-  mutate_all((~ifelse(. < 0, NA, .))) %>% 
-  summarise(wm_perc=case_when(child_age<10~rowSums(across(starts_with("wm")),na.rm=T)/6,
-                              T~rowSums(across(starts_with("wm")),na.rm=T)/10),
-            sm_perc=case_when(child_age<10~rowSums(across(starts_with("sm")),na.rm=T)/5,
-                              T~rowSums(across(starts_with("sm")),na.rm=T)/7)
-            )
+# Age < 10: 6 WM + 5 SM items
+alpha_ef_young <- e_child %>%
+  mutate(child_age = as.double(childage)) %>%
+  filter(child_age < 10) %>%
+  dplyr::select(starts_with("wm"), starts_with("sm")) %>%
+  clean_items() %>%
+  safe_alpha()
 
-cor(e_ef$wm_perc,
-    e_ef$sm_perc)
+# Age >= 10: 10 WM + 7 SM items
+alpha_ef_old <- e_child %>%
+  mutate(child_age = as.double(childage)) %>%
+  filter(child_age >= 10) %>%
+  dplyr::select(starts_with("wm"), starts_with("sm")) %>%
+  clean_items() %>%
+  safe_alpha()
 
-############################################### Putting outcome data together ######################################
-categories<-c('e_lit','e_num','e_sel','e_ef') %>% 
-  as_tibble() %>% 
-  rename('df'=value)
 
-alpha<-pmap_dfr(categories,
-           get_alpha)
+##########################################################################################
+################################## Compile Results #######################################
+##########################################################################################
+
+# Literacy is age-invariant so has one row.
+# SEL, Numeracy, and EF are split by age group due to different item sets.
+
+alpha_results <- tibble(
+  category  = c("SEL",           "SEL",
+                "Literacy",
+                "Numeracy",      "Numeracy",
+                "EF",            "EF"),
+  age_group = c("age < 10",      "age >= 10",
+                "all ages",
+                "age < 10",      "age >= 10",
+                "age < 10",      "age >= 10"),
+  raw_alpha = c(alpha_sel_young,  alpha_sel_old,
+                alpha_lit,
+                alpha_num_young,  alpha_num_old,
+                alpha_ef_young,   alpha_ef_old)
+) %>% 
+  group_by(category) %>% 
+  summarise(alpha=mean(raw_alpha))
+
+print(alpha_results)
+
 
 ##########################################################################################
 ################################## Exporting Relevant Data ###############################
 ##########################################################################################
 
-write_xlsx(alpha, "/Users/AllanLee/Desktop/Personal Projects/ECON4900/Output/01_sum_stat/03_cronbach_alpha/cronbach_alpha.xlsx")
-
-
-
-
-
+write_xlsx(alpha_results, "/Users/AllanLee/Desktop/Personal Projects/ECON4900/Output/01_sum_stat/03_cronbach_alpha/cronbach_alpha.xlsx")
